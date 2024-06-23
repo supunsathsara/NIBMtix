0dd6b26c51880942663f34b949a2174dc9a1313c
Savindu Supun <contact@supunsathsara.com>
2024-06-23 15:41:29 +0530

    Add Hero section ✨

---
+++ src/components/Hero.tsx
@@ -0,0 +1,75 @@
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { GeistMono } from "geist/font/mono";
+//use custom font from google
+
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      {/**
+       *  UI: grid
+       *  change bg color to bg-black-100 and reduce grid color from
+       *  0.2 to 0.03
+       */}
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.05] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Your Event, Your Ticket, Your Way ✨
+          </p>
+
+          <h1 className={`text-3xl md:text-4xl lg:text-7xl font-bold text-center dark:text-white text-black mt-2 tracking-widest uppercase`}>
+            NIBMT
+            <span className="text-purple dark:text-purple">I</span>X
+          </h1>
+
+          <TextGenerateEffect
+            words="Revolutionizing Event Ticketing at NIBM: Convenience, Efficiency, and Innovation"
+            className="text-center text-[20px] md:text-3xl lg:text-4xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-xs md:text-md lg:text-xl">
+            The most convenient way to buy tickets for your favorite events.
+          </p>
+
+          <a href="#">
+            <MagicButton
+              title="Register Now"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+import { ReactElement } from "react";
+import { FaHome } from "react-icons/fa";
+
+
+export const navItems= [
+  { name: "Home", link: "#home" },
+  { name: "About", link: "#about" },
+  { name: "Contact", link: "#contact" },
+  {name:"Login", link:"/login"},
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
