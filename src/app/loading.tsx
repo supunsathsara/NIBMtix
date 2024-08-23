5a84aae0b6526d56e95ab6d26874b3a22aa00a6e
Savindu Supun <contact@supunsathsara.com>
2024-08-23 20:19:37 +0530

    feat: Add loading components for dashboard and root
    The code changes include the addition of two loading components, `DashboardLoading` and `RootLoading`, which display a loading animation using SVG. These components are used to provide visual feedback to the user while data is being fetched or processed.

---
+++ src/app/loading.tsx
@@ -0,0 +1,50 @@
+const RootLoading = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full min-h-screen">
+      <svg
+        version="1.1"
+        id="L5"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 100 100"
+        enable-background="new 0 0 0 0"
+        xmlSpace="preserve"
+        className="w-28 h-28 mx-auto my-auto"
+      >
+        <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
+          <animateTransform
+            attributeName="transform"
+            dur="1s"
+            type="translate"
+            values="0 15 ; 0 -15; 0 15"
+            repeatCount="indefinite"
+            begin="0.1"
+          />
+        </circle>
+        <circle fill="#fff" stroke="none" cx="30" cy="50" r="6">
+          <animateTransform
+            attributeName="transform"
+            dur="1s"
+            type="translate"
+            values="0 10 ; 0 -10; 0 10"
+            repeatCount="indefinite"
+            begin="0.2"
+          />
+        </circle>
+        <circle fill="#fff" stroke="none" cx="54" cy="50" r="6">
+          <animateTransform
+            attributeName="transform"
+            dur="1s"
+            type="translate"
+            values="0 5 ; 0 -5; 0 5"
+            repeatCount="indefinite"
+            begin="0.3"
+          />
+        </circle>
+      </svg>
+    </div>
+  );
+};
+export default RootLoading;
