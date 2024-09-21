3f4e7d59bd00e51457f1549f425f697248b1a9eb
Savindu Supun <contact@supunsathsara.com>
2024-09-21 19:17:32 +0530

    refactor: Update Auth module to add deleteAccount function
    - Add deleteAccount function to Auth module to handle account deletion
    - Use supabaseAdmin to delete user account using admin privileges
    - Display confirmation dialog before deleting the account
    - Update DeleteAccount component to call deleteAccount function on button click

--- src/actions/Auth.ts
+++ src/actions/Auth.ts
@@ -1,14 +1,29 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
+import { supabase as supabaseAdmin } from "@/utils/supabase/serviceUser";
 import { redirect } from "next/navigation";
 
-
 export const signOut = async () => {
-console.log("signing out")
+  console.log("signing out");
   const supabase = createClient();
   await supabase.auth.signOut();
   return redirect("/login");
 };
 
+export const deleteAccount = async () => {
+  const supabase = createClient();
+  const session = await supabase.auth.getUser();
+  const userId = session.data.user?.id;
+  if (!userId) {
+    console.error("No user id found");
+    return;
+  }
+  const { data, error } = await supabaseAdmin.auth.admin.deleteUser(userId);
 
+  if (error) {
+    console.error("Error deleting user", error.message);
+    return;
+  }
+  return redirect("/login");
+};
--- src/components/DeleteAccount.tsx
+++ src/components/DeleteAccount.tsx
@@ -1,13 +1,16 @@
+"use client";
+
+import { deleteAccount } from "@/actions/Auth";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -15,40 +18,45 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 const DeleteAccount = () => {
   return (
     <Card className="bg-red-200/20 ring-red-500 border border-red-500 text-white">
-            <CardHeader>
-              <CardTitle>Delete Account</CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-4">
-              <p>
-                Deleting your account is a permanent action and cannot be
-                undone. All your data will be permanently deleted.
-              </p>
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Button variant="destructive" className="">
-                    Delete Account
-                  </Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent className="mx-1">
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      Are you sure you want to delete your account?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      This action cannot be undone. All your data will be
-                      permanently deleted.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction className="bg-destructive text-destructive-foreground hover:bg-destructive/90">
-                      Delete Account
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </CardContent>
-          </Card>
-  )
-}
-export default DeleteAccount
+      <CardHeader>
+        <CardTitle>Delete Account</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <p>
+          Deleting your account is a permanent action and cannot be undone. All
+          your data will be permanently deleted.
+        </p>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive" className="">
+              Delete Account
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent className="mx-1">
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure you want to delete your account?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. All your data will be permanently
+                deleted.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+                onClick={async () => {
+                  await deleteAccount();
+                }}
+              >
+                Delete Account
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </CardContent>
+    </Card>
+  );
+};
+export default DeleteAccount;
