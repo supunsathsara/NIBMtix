989daa4962219b67aed0adb0b97df4797ce14393
Savindu Supun <contact@supunsathsara.com>
2024-09-28 01:57:38 +0530

    feat: Add Interactive badge to auth screens

--- src/app/(Auth)/layout.tsx
+++ src/app/(Auth)/layout.tsx
@@ -1,26 +1,16 @@
-import Image from "next/image";
-import Link from "next/link";
-
+import InteractiveBadge from "@/components/InteractiveBadge";
 
 export default function AuthLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-screen">
+      <div className="hidden bg-[#180d3b]/70 lg:block">
+        <InteractiveBadge />
       </div>
-      <div className="flex items-center justify-center py-12">
-      {children}
-      </div>
+      <div className="flex items-center justify-center py-12">{children}</div>
     </div>
   );
 }
